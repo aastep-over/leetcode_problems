70725bf4f846954b86969ca38e95d65a764b7aee
~AliA <amjad.ali.connect@gmail.com>
2025-11-27 21:50:32 +0100

    # remove_element

--- remove_element.py
+++ remove_element.py
@@ -1,3 +1,4 @@
+
 # TODO: Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 from typing import *
