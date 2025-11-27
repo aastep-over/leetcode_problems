a80cae8c142027c302c48ce234a762011a38f8d7
~AliA <amjad.ali.connect@gmail.com>
2025-11-27 21:49:13 +0100

    # path_sum

--- path_sum.py
+++ path_sum.py
@@ -1,3 +1,4 @@
+
 # TODO: Given the root of a binary tree and an integer targetSum,
 # return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
 
