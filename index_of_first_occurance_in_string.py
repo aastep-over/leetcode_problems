ed429639b08311cbd7e825f98ff6f16b15a814b6
~AliA <amjad.ali.connect@gmail.com>
2025-11-27 21:31:05 +0100

    # index_of_first_occurance_in_string

--- index_of_first_occurance_in_string.py
+++ index_of_first_occurance_in_string.py
@@ -1,3 +1,4 @@
+
 # TODO: Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 class Solution:
     def faststrStr(self, haystack: str, needle: str) -> int:
