caa80bf528bb1084817e7f32294a2b7867316c28
~AliA <amjad.ali.connect@gmail.com>
2025-11-27 21:51:48 +0100

    # search_insert_position

--- search_insert_position.py
+++ search_insert_position.py
@@ -1,3 +1,4 @@
+
 # TODO: Given a sorted array of distinct integers and a target value, return the index if the target is found. 
 #       If not, return the index where it would be if it were inserted in order in O(log n)
 
